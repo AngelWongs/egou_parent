b995a4f962bb1954462a7b49134a4e5b2d0df86d
AngelWongs <38316188+AngelWongs@users.noreply.github.com>
2019-02-25 10:45:47 +0800

    egouV1.01

---
+++ egou_config_service/src/main/java/cn/ken/egou/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package cn.ken.egou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Hello world!
+ * github远程仓库配置服务中心
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
